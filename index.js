b2f8b9dd659839572432eedf98b7e6f8ad132e14
Nadiawarda <144869724+Nadiawarda@users.noreply.github.com>
2024-03-28 09:39:34 +0500

    modified

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 #! /usr/bin/env node
 import inquirer from 'inquirer';
+console.log("************** Welcom To My Calculator *************");
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
     {
         message: "Select one of the Operator to perform operator",
         type: "list",
@@ -10,7 +11,6 @@ const answer = await inquirer.prompt([
         choices: ["Addition", "Subtraction", "Multiplication", "Division"],
     },
 ]);
-console.log(answer);
 // condiional statement
 if (answer.operator === "Addition") {
     console.log(answer.firstNumber + answer.secondNumber);
@@ -27,3 +27,4 @@ else if (answer.operator === "Division") {
 else {
     console.log("Please select valid operator");
 }
+console.log("**************** Thanks Using My Calculator ***************");
--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@
 
 import inquirer from 'inquirer';
 
+console.log("************** Welcom To My Calculator *************");
+
 const answer = await inquirer.prompt([
     { message: "Enter your first number", type: "number", name: "firstNumber" },    
     { message: "Enter your second number", type: "number", name: "secondNumber" },
@@ -18,16 +20,21 @@ console.log(answer);
 
 if (answer.operator === "Addition") {
     console.log(answer.firstNumber + answer.secondNumber);
+
 } else if (answer.operator === "Subtraction") {
     console.log(answer.firstNumber - answer.secondNumber);
+
 } else if (answer.operator === "Multiplication"){
     console.log(answer.firstNumber * answer.secondNumber);
+
 } else if (answer.operator === "Division"){
     console.log(answer.firstNumber / answer.secondNumber);
-} else {
+
+} else  {
     console.log("Please select valid operator");
-}
+
+} 
+    console.log("**************** Thanks Using My Calculator ***************");
 
 
 
-console.log("The End");
